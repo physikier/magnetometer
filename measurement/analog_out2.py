7ca610f60c12ec8fc1132f851e43236c02ff27ea
Physikier <phy76371@stud.uni-stuttgart.de>
2016-05-19 17:51:36 +0200

    code works now for high freq output

--- measurement/analog_out2.py
+++ measurement/analog_out2.py
@@ -18,10 +18,18 @@ class WaveformThread(threading.Thread):
     self.data=waveform
     sampsPerChanWritten=int32()
 
-    self.CHK(DAQmxCreateTask(b'',byref(self.taskHandle)))
+    self.CHK(DAQmxCreateTask(b'',self.taskHandle))
     self.CHK(DAQmxCreateAOVoltageChan(self.taskHandle,b'/Dev2/ao0',b'',-5.,5.,DAQmx_Val_Volts,None))
-    print(DAQmx_Val_FiniteSamps)
-    self.CHK(DAQmxCfgSampClkTiming(self.taskHandle, b'', self.sampleRate, DAQmx_Val_Rising, DAQmx_Val_ContSamps, self.periodLength));
+    self.CHK(DAQmxCfgSampClkTiming(self.taskHandle, b'', self.sampleRate, DAQmx_Val_Rising, DAQmx_Val_ContSamps, self.periodLength))
+    #self.CHK(DAQmxSetWriteAttribute(self.taskHandle, DAQmx_Write_RegenMode, DAQmx_Val_AllowRegen))
+    # #self.CHK(DAQmxCfgPipelinedSampClkTiming(self.taskHandle, b'', 2e6, DAQmx_Val_Rising, DAQmx_Val_ContSamps, self.periodLength))
+    # self.CHK(DAQmxCfgOutputBuffer(self.taskHandle, 2000000))
+    # #self.CHK(DAQmxSetAODataXferReqCond(self.taskHandle,b'/Dev2/ao0',10240))
+    # self.CHK(DAQmxSetAODataXferMech(self.taskHandle, b'/Dev2/ao0',DAQmx_Val_USBbulk))
+    # #self.CHK(DAQmxCfgInputBuffer(self.taskHandle, 1))
+    # #print(DAQmx_Val_FiniteSamps)
+    
+    
     self.CHK(DAQmxWriteAnalogF64(self.taskHandle, self.periodLength, 0, 100.0, DAQmx_Val_GroupByChannel, self.data, None, None))
     threading.Thread.__init__(self)
 
@@ -54,9 +62,9 @@ class WaveformThread(threading.Thread):
       DAQmxClearTask(self.taskHandle)
 
 def func_ramp(amplitude, period, samples):
-    t = numpy.arange(0, period, 1.0/samples)
+    t = numpy.arange(0, period, 1.0/samples, endpoint=False)
     period_half_int = int(len(t)/2)
-    print('periodhalfint: ' + str(period_half_int))
+    #print('periodhalfint: ' + str(period_half_int))
     x = numpy.array([])
     for i in range(len(t)):
       if i <= period_half_int:
@@ -72,13 +80,13 @@ def func_ramp(amplitude, period, samples):
         #print(x[i])
         x = numpy.append(x, [(-2*amplitude/period)*t[i] + 3*amplitude/2])
         #print('P=' +str(x[-1]))
-        print(t[-1])
+        #print(t[-1])
         #print()
-    print(x, t)
+    #print(x, t)
     return x, samples
 
 def func_sin(period, samples):
-    t = numpy.arange(0, period, 1.0/samples)
+    t = numpy.arange(0, 50*period, 1.0/samples)
     x = numpy.sin(2*numpy.pi*t/period)
     return x, samples
 
@@ -95,12 +103,12 @@ if __name__ == '__main__':
   #x = func_ramp(t, 2)
 
 
-  #x, samplerate = func_sin(1, 100)
-  x, samplerate = func_ramp(3, .5, 1000)
+  x, samplerate = func_sin(0.00001, 2.6e6)
+  #x, samplerate = func_ramp(3, 0.001, 2e6)
 
-  print(type(x))
+  #print(type(x))
   # mythread = WaveformThread( x, 100 )
   mythread = WaveformThread( x, samplerate)
   mythread.start()
-  time.sleep(10)
+  time.sleep(5)
   mythread.stop()
