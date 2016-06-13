27ea14fd622723f5285fa005fd06ea59372c4910
physikier <dani.arnold@gmx.de>
2016-06-13 17:25:43 +0200

    some changes

--- measurement/test.py
+++ measurement/test.py
@@ -1,3 +1,31 @@
-import yaml_writer as yw
+# import yaml_writer as yw
 
-yw.yaml_writer_hanle()
+# yw.yaml_writer_hanle()
+
+import numpy
+
+# freq=1000
+# samples=200000
+# t = numpy.arange(0, (1./freq), 1.0/samples)
+# #print('t', t.size)
+
+# #init_t = numpy.array([])
+# init_t = t
+# #test = numpy.array([])
+# for x in range(0,3):
+# 		test = []
+# 		test.append(x)
+
+# #print(x)
+
+# print(test)
+# #print('init_t', init_t.size)
+# # for i in range(1,10):
+# #  	print('i in for', i)
+# #     numpy.append(init_t, t)
+    
+# #print('size init t:', init_t.size)
+
+data = numpy.zeros(100)
+data = numpy.zeros(30)
+print(data)
